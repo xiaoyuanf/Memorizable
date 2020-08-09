d69ecf3c69755ccd225526f42006bc4b1e645f4a
Margot Chen <c3xiaoy@gmail.com>
2020-08-09 16:51:53 -0700

    finished deleting a deck

--- src/main/ui/gui/QueueGUI.java
+++ src/main/ui/gui/QueueGUI.java
@@ -5,6 +5,7 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
 // adapted from https://docs.oracle.com/javase/tutorial/uiswing/examples/components/ListDemoProject/src/components/ListDemo.java
@@ -13,10 +14,11 @@ public class QueueGUI extends JPanel implements ListSelectionListener {
     private MemorizableGUI memoGUI;
     private JList queues;
     private DefaultListModel listModel;
-    private static final String addQueueString = "Add a new deck";
-    private static final String delQueueString = "Delete the deck";
+    //private static final String addQueueString = "Add a new deck";
+    //private static final String delQueueString = "Delete the deck";
     private JButton addQueueButton;
     private JButton delQueueButton;
+    private JButton reviewQueueButton;
 
     public QueueGUI(MemorizableGUI memoGUI) {
         this.memoGUI = memoGUI;
@@ -24,28 +26,69 @@ public class QueueGUI extends JPanel implements ListSelectionListener {
         createButtons();
     }
 
+    private class DelListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            //This method can be called only if
+            //there's a valid selection
+            //so go ahead and remove whatever's selected.
+            int index = queues.getSelectedIndex();
+            listModel.remove(index);
+
+            int size = listModel.getSize();
+
+            if (size == 0) { //Nobody's left, disable delete.
+                delQueueButton.setEnabled(false);
+
+            } else { //Select an index.
+                if (index == listModel.getSize()) {
+                    //removed item in last position
+                    index--;
+                }
+
+                queues.setSelectedIndex(index);
+                queues.ensureIndexIsVisible(index);
+            }
+        }
+    }
+
     @Override
     public void valueChanged(ListSelectionEvent e) {
+        if (e.getValueIsAdjusting() == false) {
 
+            if (queues.getSelectedIndex() == -1) {
+                //No selection, disable the delete button.
+                delQueueButton.setEnabled(false);
+
+            } else {
+                //Selection, enable the delete button.
+                delQueueButton.setEnabled(true);
+            }
+        }
     }
 
     private void createButtons() {
-        JButton addQueueButton = new JButton("Add a new deck");
+        this.addQueueButton = new JButton("Add a new deck");
+        this.delQueueButton = new JButton("Delete the deck");
+        delQueueButton.setActionCommand("delete");
+        delQueueButton.addActionListener(new DelListener());
 
-        JButton delQueueButton = new JButton("Delete the deck");
 
-        addQueueButton.setActionCommand("add");
-        addQueueButton.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //TODO
-            }
-        });
+        this.reviewQueueButton = new JButton("Review the deck");
+
+//        addQueueButton.setActionCommand("add");
+//        addQueueButton.addActionListener(new AbstractAction() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                //TODO
+//            }
+//        });
 
         JPanel buttonsPane = new JPanel();
         buttonsPane.setLayout(new FlowLayout());
-        buttonsPane.add(addQueueButton);
+        buttonsPane.add(reviewQueueButton);
         buttonsPane.add(delQueueButton);
+        buttonsPane.add(addQueueButton);
+
 
         buttonsPane.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
 
@@ -72,7 +115,7 @@ public class QueueGUI extends JPanel implements ListSelectionListener {
 
         JPanel queuesPane = new JPanel();
         queuesPane.setLayout(new BoxLayout(queuesPane, BoxLayout.PAGE_AXIS));
-        JLabel queueLabel = new JLabel("Pick a deck to review");
+        JLabel queueLabel = new JLabel("Pick a deck");
         queueLabel.setLabelFor(queues);
 
         queuesPane.add(queueLabel);
